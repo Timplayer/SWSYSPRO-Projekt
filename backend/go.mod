3141fad92853347efb646b8d22bd7a5ecace372f
niko4u5 <Niklas@fam-email.de>
2024-05-26 16:35:38 +0200

    test token validation

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,25 @@ module backend.hivedrive.de/server
 
 go 1.21.9
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/zitadel/oidc/v3 v3.24.0
+)
+
+require (
+	github.com/go-jose/go-jose/v4 v4.0.2 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+	github.com/muhlemmer/gu v0.3.1 // indirect
+	github.com/zitadel/logging v0.6.0 // indirect
+	github.com/zitadel/schema v1.3.0 // indirect
+	go.opentelemetry.io/otel v1.26.0 // indirect
+	go.opentelemetry.io/otel/metric v1.26.0 // indirect
+	go.opentelemetry.io/otel/trace v1.26.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/oauth2 v0.20.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,16 +1,34 @@
 package main
 
 import (
+	"context"
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"log"
 	"net/http"
+	"strings"
+	"time"
+
+	"github.com/zitadel/oidc/v3/pkg/client/rs"
+	"github.com/zitadel/oidc/v3/pkg/oidc"
 )
 
 func main() {
+	//keyPath := os.Getenv("KEY")
+	issuer := "http://keycloak:8080/auth/realms/hivedrive"
+	clientID := "go-backend"
+	clientSecret := "B9BzwdWt0oFJZHfGgrammwW0THj380Ig"
+
+	provider, err := rs.NewResourceServerClientCredentials(context.TODO(), issuer, clientID, clientSecret)
+	if err != nil {
+		logrus.Fatalf("error creating provider %s", err.Error())
+	}
+
 	//create router
 	router := mux.NewRouter()
 	//add routes
-	router.HandleFunc("/api/hello", hello()).Methods("GET")
+	router.HandleFunc("/api/test/hello", hello()).Methods("GET")
+	router.HandleFunc("/api/test/auth", auth(provider)).Methods("GET")
 
 	//start server
 	log.Fatal(http.ListenAndServe(":80", router))
@@ -21,4 +39,39 @@ func hello() http.HandlerFunc {
 		writer.WriteHeader(http.StatusOK)
 		writer.Write([]byte("hello world"))
 	}
+}
+
+func auth(provider rs.ResourceServer) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ok, token := checkToken(w, r)
+		if !ok {
+			return
+		}
+		resp, err := rs.Introspect[*oidc.IntrospectionResponse](r.Context(), provider, token)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusForbidden)
+			return
+		}
+
+		value, ok := resp.Claims["user_id"].(string)
+		if !ok || value == "" {
+			http.Error(w, "claim does not match", http.StatusForbidden)
+			return
+		}
+		w.Write([]byte("authorized with value " + value))
+	}
+
+}
+
+func checkToken(w http.ResponseWriter, r *http.Request) (bool, string) {
+	auth := r.Header.Get("authorization")
+	if auth == "" {
+		http.Error(w, "auth header missing", http.StatusUnauthorized)
+		return false, ""
+	}
+	if !strings.HasPrefix(auth, oidc.PrefixBearer) {
+		http.Error(w, "invalid header", http.StatusUnauthorized)
+		return false, ""
+	}
+	return true, strings.TrimPrefix(auth, oidc.PrefixBearer)
 }
