ba9c13847f985f468768fc77835e90ca4bc8a8de
niko4u5 <Niklas@fam-email.de>
2024-05-30 21:12:02 +0200

    Backend: add oauth integration test

---
+++ backend/test/oauth-integration.sh
@@ -0,0 +1,16 @@
+set -o errexit
+client="admin-cli"
+memberName="member_test"
+memberSecret="test"
+
+token=$(curl -f -k \
+-d "client_id=${client}" \
+-d "username=${memberName}" \
+-d "password=${memberSecret}" \
+-d "grant_type=password" \
+https://localhost:8080/auth/realms/hivedrive/protocol/openid-connect/token \
+| jq .access_token)
+
+curl -f -k \
+-H "Authorization: Bearer $token" \
+https://localhost:8080/api/healthcheck/auth
