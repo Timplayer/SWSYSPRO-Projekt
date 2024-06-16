25125f01b09472ec5e4d294898b9c2c2f2544fca
Tim Jaeschke <tim.jae@gmx.de>
2024-06-16 14:30:21 +0200

    Added Account Management Page

---
+++ frontend/hivedrive/src/pages/admin/Account.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Box, Typography, Button } from '@mui/material';
+
+const Account: React.FC = () => {
+    const handleAccountManagement = () => {
+        //window.location.href = keycloak.createAccountUrl();
+    };
+
+    return (
+        <Box sx={{ p: 3 }}>
+            <Typography variant="h4" gutterBottom>
+                Account Management
+            </Typography>
+            <Button variant="contained" color="primary" onClick={handleAccountManagement}>
+                Manage Account
+            </Button>
+        </Box>
+    );
+};
+
+export default Account;
