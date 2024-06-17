d6bff39b0756f58ab82b4d92cbd9316c39b66d26
AlphaLEXray <alphalexray@protonmail.com>
2024-06-17 14:15:39 +0200

    Add vehicle_categories.go

---
+++ backend/vehicle_categories.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"net/http"
+)
+
+type vehicleCategoriy struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+func getVehicleCategoryById(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		rows, err := dbpool.Query(context.Background(), "SELECT * FROM vehicle_categories WHERE vehicle_category_id = $1",
+			mux.Vars(request)["id"])
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error finding vehicle_categories: %v\n", err)
+		}
+		defer rows.Close()
+
+		if rows.Next() {
+			var vC vehicleCategoriy
+			err = rows.Scan(&vC.Id, &vC.Name)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error finding vehicle_categories: %v\n", err)
+				return
+			}
+			str, err := json.Marshal(vC)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error finding vehicle_categories: %v\n", err)
+				return
+			}
+			writer.Header().Set("Content-Type", "application/json")
+			writer.Write(str)
+		}
+	}
+}
