568b8c1c0333ae8c6ef3dd2abd2e67f60c1424a4
AlphaLEXray <alphalexray@protonmail.com>
2024-06-23 13:29:52 +0200

    Add postImage function

--- backend/images.go
+++ backend/images.go
@@ -1,16 +1,57 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"io"
 	"log"
 	"net/http"
 )
 
-type image struct {
-	Id       int64     `json:"id"`
-	FileName string    `json:"file_name"`
-	File     http.File `json:"file"`
+type picture struct {
+	Id       int64  `json:"id"`
+	FileName string `json:"file_name"`
+	File     []byte `json:"file"`
+}
+
+func postImage(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+
+		err := request.ParseMultipartForm(1000) // maxMemory in MB
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error parsing multipart form: %v\n", err)
+			return
+		}
+
+		file, header, err := request.FormFile("file")
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error parsing file: %v\n", err)
+			return
+		}
+
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, file); err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error reading file: %v\n", err)
+			return
+		}
+
+		rows, err := dbpool.Query(context.Background(),
+			"INSERT INTO images (fileName, file) VALUES ($1, $2) RETURNING id", header.Filename, buf.Bytes())
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert image: %v", err)
+			return
+		}
+		defer rows.Close()
+
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusCreated)
+		writer.Write(nil)
+	}
 }
 
 func createImagesTable(dbpool *pgxpool.Pool) {
