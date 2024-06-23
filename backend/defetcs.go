de605f224c9b75fd916955fd1b12b2397f84ac49
AlphaLEXray <alphalexray@protonmail.com>
2024-06-23 14:26:11 +0200

    Add postDefect function

--- backend/defetcs.go
+++ backend/defetcs.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"io"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -12,6 +15,52 @@ type defect struct {
 	Name        string    `json:"name"`
 	Date        time.Time `json:"date"`
 	Description string    `json:"description"`
+}
+
+func postDefect(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		body, err := io.ReadAll(request.Body)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error reading request body: %v\n", err)
+			return
+		}
+		var d defect
+		err = json.Unmarshal(body, &d)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error reading request body: %v\n", err)
+			return
+		}
+		rows, err := dbpool.Query(context.Background(),
+			"INSERT INTO defects (name, date, description) VALUES ($1, $2, $3) RETURNING id",
+			d.Name, d.Date, d.Description)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert defect: %v", err)
+			return
+		}
+		defer rows.Close()
+		rows.Next()
+		var id int64
+		err = rows.Scan(&id)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert defect: %v", err)
+			return
+		}
+		log.Printf("Inserted defect: %d", id)
+		d.Id = id
+		body, err = json.Marshal(d)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error serializing defect: %v", err)
+			return
+		}
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusCreated)
+		writer.Write(body)
+	}
 }
 
 func createDefectsTable(dbpool *pgxpool.Pool) {
