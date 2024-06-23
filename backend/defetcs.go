4dedc67ed501b964a7bdc023c7d68b9af3ee9b23
AlphaLEXray <alphalexray@protonmail.com>
2024-06-23 09:38:50 +0200

    Remove pictures from struct

--- backend/defetcs.go
+++ backend/defetcs.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"image"
 	"time"
 )
 
 type defect struct {
-	Id          int64       `json:"id"`
-	Name        string      `json:"name"`
-	Date        time.Time   `json:"date"`
-	Description string      `json:"description"`
-	Pictures    image.Image `json:"pictures"`
+	Id          int64     `json:"id"`
+	Name        string    `json:"name"`
+	Date        time.Time `json:"date"`
+	Description string    `json:"description"`
 }
