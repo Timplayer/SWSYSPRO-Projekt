395b121b65d1d2c5a21a263542e1168c51b8abb1
AlphaLEXray <alphalexray@protonmail.com>
2024-06-23 09:46:42 +0200

    Add createImagesTable function

--- backend/images.go
+++ backend/images.go
@@ -1,9 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+	"net/http"
+)
 
 type image struct {
 	Id       int64     `json:"id"`
 	FileName string    `json:"file_name"`
 	File     http.File `json:"file"`
+}
+
+func createImagesTable(dbpool *pgxpool.Pool) {
+	_, err := dbpool.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS images (id BIGSERIAL PRIMARY KEY, fileName TEXT, file bytea)")
+	if err != nil {
+		log.Fatalf("Failed to create table: %v\n", err)
+	}
 }
