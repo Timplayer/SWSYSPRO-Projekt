fce378950ff41c2b5e1f54a8c3560b45798af3ee
AlphaLEXray <alphalexray@protonmail.com>
2024-06-23 09:17:27 +0200

    Add struct to defects.go

--- backend/defetcs.go
+++ backend/defetcs.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"image"
+	"time"
+)
+
+type defect struct {
+	Id          int64       `json:"id"`
+	Name        string      `json:"name"`
+	Date        time.Time   `json:"date"`
+	Description string      `json:"description"`
+	Pictures    image.Image `json:"pictures"`
+}
