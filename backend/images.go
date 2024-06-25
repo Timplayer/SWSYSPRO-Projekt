43ed21834370cb8bb43900171266e86867d99300
AlphaLEXray <alphalexray@protonmail.com>
2024-06-25 12:04:47 +0200

    Add postVehicleImage

--- backend/images.go
+++ backend/images.go
@@ -88,6 +88,78 @@ func postImage(dbpool *pgxpool.Pool) http.HandlerFunc {
 	}
 }
 
+func postVehicleImage(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+
+		err := request.ParseMultipartForm(1000) // maxMemory in MB
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error parsing multipart form: %v\n", err)
+			return
+		}
+
+		file, header, err := request.FormFile("file")
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error parsing file: %v\n", err)
+			return
+		}
+
+		buf := bytes.NewBuffer(nil)
+		if _, err := io.Copy(buf, file); err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error reading file: %v\n", err)
+			return
+		}
+
+		rows, err := dbpool.Query(context.Background(),
+			"INSERT INTO images (fileName, file) VALUES ($1, $2) RETURNING id;", header.Filename, buf.Bytes())
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert image: %v", err)
+			return
+		}
+		defer rows.Close()
+
+		var p picture
+		rows.Next()
+		err = rows.Scan(&p.Id)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert image (rows.Scan): %v", err)
+			return
+		}
+		_, err = dbpool.Query(context.Background(),
+			"UPDATE images SET url = $1 WHERE id = $2;", "https://"+request.Host+request.URL.Path+"/file/id/"+strconv.FormatInt(p.Id, 10), p.Id)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing update image: %v", err)
+			return
+		}
+
+		_, err = dbpool.Query(context.Background(),
+			"INSERT INTO vehicleImage (vehicleId, imageId) VALUES ($1, $2);", mux.Vars(request)["id"], p.Id)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error creating connection: %v", err)
+			return
+		}
+
+		var body []byte
+		body, err = json.Marshal(p)
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing insert image (json.Marshal): %v", err)
+			return
+		}
+
+		log.Printf("Image inserted: %d", p.Id)
+		writer.Header().Set("Content-Type", "application/json")
+		writer.WriteHeader(http.StatusCreated)
+		writer.Write(body)
+	}
+}
+
 func getImageById(dbpool *pgxpool.Pool) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		rows, err := dbpool.Query(context.Background(), "SELECT url FROM images WHERE id = $1", mux.Vars(request)["id"])
