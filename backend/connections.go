8afe2bd647947e5bd69cc827451f1d32c5c04bb8
AlphaLEXray <alphalexray@protonmail.com>
2024-06-25 11:06:24 +0200

    Add createVehicleImageTable function

--- backend/connections.go
+++ backend/connections.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"context"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"log"
+)
+
 type Connection struct {
 	EntityId int64 `json:"entityId"`
 	ImageId  int64 `json:"imageId"`
+}
+
+func createVehicleImageTable(dbpool *pgxpool.Pool) {
+	_, err := dbpool.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS vehicleImage (vehicleId BIGSERIAL references vehicles(id), imageId BIGSERIAL references images(id))")
+	if err != nil {
+		log.Fatalf("Failed to create table: %v\n", err)
+	}
 }
