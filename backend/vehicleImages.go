536a99cd75a52aa6c156e5f3872715a0544345a4
AlphaLEXray <alphalexray@protonmail.com>
2024-06-25 16:11:36 +0200

    Add deleteVehicleImage function

--- backend/vehicleImages.go
+++ backend/vehicleImages.go
@@ -95,6 +95,52 @@ func postVehicleImage(dbpool *pgxpool.Pool) http.HandlerFunc {
 	}
 }
 
+func deleteVehicleImage(dbpool *pgxpool.Pool) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		rows, err := dbpool.Query(context.Background(),
+			"DELETE FROM vehicleImage WHERE imageId = $1;", mux.Vars(request)["id"])
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing delete image: %v", err)
+			return
+		}
+		rows, err = dbpool.Query(context.Background(),
+			"DELETE FROM images WHERE id = $1 RETURNING images.id;", mux.Vars(request)["id"])
+		if err != nil {
+			writer.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error executing delete image: %v", err)
+			return
+		}
+		defer rows.Close()
+
+		if rows.Next() {
+			var p picture
+			err = rows.Scan(&p.Id)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error deleting image: %p\n", err)
+				return
+			}
+			str, err := json.Marshal(p)
+			if err != nil {
+				writer.WriteHeader(http.StatusInternalServerError)
+				log.Printf("Error deleting image: %p\n", err)
+				return
+			}
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(http.StatusOK)
+			writer.Write(str)
+			return
+		}
+
+		if !rows.Next() {
+			writer.WriteHeader(http.StatusNotFound)
+			log.Printf("Error deleting image: image not found \n")
+			return
+		}
+	}
+}
+
 func getVehicleImagesByVehicleId(dbpool *pgxpool.Pool) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		rows, err := dbpool.Query(context.Background(), "SELECT images.url FROM vehicles JOIN vehicleImage ON vehicles.id = vehicleImage.vehicleId JOIN images ON vehicleImage.imageId = images.id WHERE vehicles.id = $1", mux.Vars(request)["id"])
