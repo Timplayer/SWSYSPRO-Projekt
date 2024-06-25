b9ad783f94279582adbc9493ca2a188027d8f6df
AlphaLEXray <alphalexray@protonmail.com>
2024-06-25 11:11:36 +0200

    Remove struct Connection

--- backend/connections.go
+++ backend/connections.go
@@ -6,11 +6,6 @@ import (
 	"log"
 )
 
-type Connection struct {
-	EntityId int64 `json:"entityId"`
-	ImageId  int64 `json:"imageId"`
-}
-
 func createVehicleImageTable(dbpool *pgxpool.Pool) {
 	_, err := dbpool.Exec(context.Background(), "CREATE TABLE IF NOT EXISTS vehicleImage (vehicleId BIGSERIAL references vehicles(id), imageId BIGSERIAL references images(id))")
 	if err != nil {
