4ecb6f4d78c6d2ac8bfd23698e3d9c74cf5bd47f
Thxrsty <145643083+Thxrsty@users.noreply.github.com>
2024-06-27 11:16:01 +0200

    open source vorbereitung

--- frontend/hivedrive/src/pages/OpenSourcethings.tsx
+++ frontend/hivedrive/src/pages/OpenSourcethings.tsx
@@ -4,15 +4,29 @@ import Typography from '../components/Typography';
 import AppAppBar from '../views/AppAppBar';
 import AppFooter from '../views/AppFooter';
 import withRoot from '../withRoot';
+import opensourcedata from '../data/OpenSource.json';
+import { Box } from '@mui/material';
 
 function OpenSourcethings() {
   return (
     <React.Fragment>
       <AppAppBar />
       <Container>
-        <Typography color={'#FFFFFF'}>
-            coming soon
-        </Typography>
+        <Box sx={{ mt: 7, mb: 12, color: '#ffffff' }}>
+          <Typography variant="h3" gutterBottom marked="center" align="center" sx={{ color: '#ff9800' }}>
+            {opensourcedata.title}
+          </Typography>
+          {opensourcedata.sections.map((section, index) => (
+            <Box key={index} sx={{ mt: 5 }}>
+              <Typography variant="h5" gutterBottom>
+                {section.heading}
+              </Typography>
+              <Typography variant="body1">
+                {section.content}
+              </Typography>
+            </Box>
+          ))}
+        </Box>
       </Container>
       <AppFooter />
     </React.Fragment>
