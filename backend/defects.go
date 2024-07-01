e741a7696fbebb18bad17034700518454cfaf0fe
AlphaLEXray <alphalexray@protonmail.com>
2024-07-01 09:53:42 +0200

    Change order of attributes

--- backend/defects.go
+++ backend/defects.go
@@ -58,8 +58,8 @@ func getDefectByID(dbpool *pgxpool.Pool) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		var d defect
 		err := dbpool.QueryRow(context.Background(),
-			"SELECT userId, id, name, date, description, status FROM defects WHERE defects.id = $1",
-			mux.Vars(request)["id"]).Scan(&d.UserId, &d.Id, &d.Name, &d.Date, &d.Description, &d.Status)
+			"SELECT id, userId, name, date, description, status FROM defects WHERE defects.id = $1",
+			mux.Vars(request)["id"]).Scan(&d.Id, &d.UserId, &d.Name, &d.Date, &d.Description, &d.Status)
 		if errors.Is(err, pgx.ErrNoRows) {
 			writer.WriteHeader(http.StatusNotFound)
 			log.Printf(errorGenericNotFound, cDefect, cDefect)
@@ -85,7 +85,7 @@ func getDefects(dbpool *pgxpool.Pool) http.HandlerFunc {
 		defects, err := pgx.CollectRows(rows,
 			func(row pgx.CollectableRow) (defect, error) {
 				var d defect
-				err := rows.Scan(&d.UserId, &d.Id, &d.Name, &d.Date, &d.Description, &d.Status)
+				err := rows.Scan(&d.Id, &d.UserId, &d.Name, &d.Date, &d.Description, &d.Status)
 				return d, err
 			})
 		if err != nil {
