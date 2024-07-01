1bc3528b6eba32a5ad608eb3500cd757b3677b4b
AlphaLEXray <alphalexray@protonmail.com>
2024-07-01 09:19:36 +0200

    Add login check for postDefect

--- backend/main.go
+++ backend/main.go
@@ -80,7 +80,7 @@ func main() {
 	router.HandleFunc(vehiclesAPIpath, getVehicles(dbpool)).Methods("GET")
 	router.HandleFunc(vehiclesAPIpath, updateVehicle(dbpool)).Methods("PUT")
 
-	router.HandleFunc(defectsAPIpath, RestRequestWithTransaction(dbpool, http.StatusCreated, postDefect)).Methods("POST")
+	router.HandleFunc(defectsAPIpath, RestRequestWithTransaction(dbpool, http.StatusCreated, postDefect).Methods("POST")
 	router.HandleFunc(defectsAPIpath, getDefects(dbpool)).Methods("GET")
 	router.HandleFunc(defectsAPIpath, updateDefect(dbpool)).Methods("PUT")
 
