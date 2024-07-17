33a30fd6aa5a40011d3e37eeaa1e7df8b32fd2e6
AlphaLEXray <alphalexray@protonmail.com>
2024-07-17 13:08:10 +0200

    Improve contrast on NotFound page

--- frontend/hivedrive/src/pages/NotFound.tsx
+++ frontend/hivedrive/src/pages/NotFound.tsx
@@ -11,9 +11,9 @@ function NotFound() {
 
   return (
     <>
-      <h2>404 Not Found</h2>
-      <p>The page you are looking for does not exist.</p>
-      <button onClick={handleNavigateHome}>Go to Homepage</button>
+      <h2 style={{color: 'white'}}>404 Not Found</h2>
+      <p style={{color: 'white'}}>The page you are looking for does not exist.</p>
+      <button onClick={handleNavigateHome} style={{color: 'white'}}>Go to Homepage</button>
     </>
   );
 }
